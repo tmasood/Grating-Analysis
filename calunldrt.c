0b79db8920bfaffcc8373e7273a14af6782a33fa
Taha <tmasoodiqd@gmail.com>
2015-11-06 22:06:56 -0800

    Create calunldrt.c

---
+++ calunldrt.c
@@ -0,0 +1,28 @@
+//
+//  Copyright 2015 Quantum Designs LLC, Taha Masood, Johannes Tausch
+//  and Jerome Butler
+//
+//  Permission to use, copy, and distribute this software and its
+//  documentation for any purpose with or without fee is hereby granted,
+//  provided that the above copyright notice appear in all copies and
+//  that both the copyright notice and this permission notice appear
+//  in supporting documentation.
+//
+//  This software is provided "as is" without express or implied warranty
+//  to the extent permitted by applicable law.
+//
+#include <fstream>
+#include <iostream>
+#include <complex>
+
+#include "ssystem.h"
+#include "structure.h"
+#include "grating.h"
+
+using namespace std;
+
+int calcunldroot(structure *epiptr, ssystem *sysptr)
+{
+
+  return 0;
+}
